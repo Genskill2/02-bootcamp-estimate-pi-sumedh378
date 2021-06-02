6abf7e9db3a194b85ceaf31c7c98e1bc23e4b3b4
sumedh <sumedh.378@gmail.com>
2021-06-02 15:09:54 +0530

    erros fixed

--- estimate.py
+++ estimate.py
@@ -14,17 +14,17 @@ class TestWallis(unittest.TestCase):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
     def wallis(n):
+    	if(n==0):
+    		return 0
     	mul = 1.0
-    	for i in range(1,n):
-    		k = 4*i*i
+    	for i in range(0,n+1):
+    		k = 4*(i**2)
     		mul = mul*k/(k-1)
     	return mul*2
 
 
 class TestMC(unittest.TestCase):
 
-	cp= 0 #circle darts
-	sp= 0 #square darts
 
     def test_randomness(self):
         pi0 = monte_carlo(15000)
@@ -39,19 +39,17 @@ class TestMC(unittest.TestCase):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
     def monte_carlo( n ):
-    	for i in range(1,n):
+    	for i in range(0,n+1):
     		x = random(-1,1)
     		y = random(-1,1)
     		 
     		check = x*x + y*y
     		
-    		if (check <= 1) :
+    		if (check < 1) :
     			cp+=1
     			
-    		sp+=1
-    		
-    	pi = 4*(cp/sp)
-    	return pi
+    	pi = 4*(cp/n)
+    		return pi
         
     
 if __name__ == "__main__":
